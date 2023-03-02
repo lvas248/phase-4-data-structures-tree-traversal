72ac97667b70b6653f352bdabea5ac2ad8df29af
Luis <lvas248@gmail.com>
2023-03-02 13:43:41 -0500

    commit tree e388c436152a75c13b0cbcd893f64f755723a0a5

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,19 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      if node[:id] == id
+        return node
+      else
+        nodes_to_visit = node[:children] + nodes_to_visit
+      end
+    end
+    nil
+  end
+
 end
